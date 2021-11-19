6e8d419803b40190221a1e660abfa32eed0006a3
paul88vera <paul88vera@gmail.com>
2021-11-19 15:44:39 -0600

    objects/variables stated

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,117 @@
 // Assignment code here
+//  lowercase letter array
+var lowercaseLetters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
 
+// uppercase letter array
+var uppercaseLetters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
+
+var numberList = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9
+];
+
+// special character array
+var specialChar = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  ":",
+  ";",
+  ",",
+  "<",
+  ".",
+  ">",
+  "/",
+  "?",
+  "{",
+  "}",
+  "[",
+  "]",
+  "|",
+  "\\",
+  "'",
+  '"',
+  "-",
+  "_",
+  "+",
+  "=",
+  "`",
+  "~",
+  " "
+];
+
+
+
+var generatePassword = function(){};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -8,6 +120,8 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+
 
   passwordText.value = password;
 
