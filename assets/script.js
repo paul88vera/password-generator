7f1f4ae10c51044395adefa0991b41a741cb8caa
paul88vera <paul88vera@gmail.com>
2021-11-22 01:48:34 -0600

    created char arrays but booleans are messed up

--- assets/script.js
+++ assets/script.js
@@ -1,153 +1,51 @@
-// Assignment code here
+/* CHARACTER VARIABLES */
+var charLowToHigh = function(low, high) {
+    
+  var array = [];
+  for (i = low; i <= high; i++) {
+    array.push(i);
+  }
+  return array;
+}
+var specialCharacter = charLowToHigh(33, 47).concat(charLowToHigh(58, 63)).concat(charLowToHigh(91, 126));
+var numbers = charLowToHigh(48,57);
+var upperCase = charLowToHigh(65, 90);
+var lowerCase = charLowToHigh(97, 122);
 
-/* CHARACTER ARRAYS */
 
-// uppercase letter array
-var uppercaseLetters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z"
-];
-
-// lowerCase array
-var lowercaseLetters = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z"
-
-];
-
-// special character array
-var specialChar = [
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  ":",
-  ";",
-  ",",
-  "<",
-  ".",
-  ">",
-  "/",
-  "?",
-  "{",
-  "}",
-  "[",
-  "]",
-  "|",
-  "\\",
-  "'",
-  '"',
-  "-",
-  "_",
-  "+",
-  "=",
-  "`",
-  "~",
-  " "
-];
-
-/* ARRAY OF RANDOMIZED INPUT SELECTIONS */
-
-var randomArray = [
-  // 0 - this will allow random selections from specialChar array
-  specialChar[Math.floor(Math.random() * specialChar.length)],
-  
-  // 1 - this will only allow random numbers 0-9
-  Math.floor(Math.random() * 9),
-  
-  // 2 - this will allow random selections from the uppercaseLetters array
-  uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)],
-
-  // 3 - this will allow random selections from the lowercaseLetters array
-  lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)],
-
-];
-
-/* PASSWORD LENGTH VERIFICATION */
-
-var passwordLength = function(){
-
-    var passwordTotal = prompt("How long would you like your password to be? 4-40 Characters");
-
-    if  (passwordTotal === "" || passwordTotal === null) {
-      window.alert("You need to provide a valid number. Please try again. 4-40 Characters");
-      return passwordLength();
-    }
-    else if (passwordTotal >= 4 && passwordTotal <= 40) {
-      console.log("Your password total length is: " + passwordTotal);
-    }
-    else {
-      window.alert("You need to provide a valid number. Please try again. 4-40 Characters");
-      return passwordLength();
-    }
-
-  };
 
 /* GENERATE PASSWORD */
 
-generatePassword = function(){
-  
-  passwordLength(); 
+var generatePassword = function(specialCharacter, confirmNumbers, confirmUpperCase, confirmLowerCase){
+ 
+  /* PASSWORD LENGTH VERIFICATION */
+  var passwordLength = function() {var passwordTotal = prompt("How long would you like your password to be? 4-40 Characters");
 
-  // Criteria
+  if  (passwordTotal === "" || passwordTotal === null) {
+    window.alert("You need to provide a valid number. Please try again. 4-40 Characters");
+    return passwordLength();
+  }
+  else if (passwordTotal >= 4 && passwordTotal <= 40) {
+    console.log("Your password total length is: " + passwordTotal);
+  }
+  else {
+    window.alert("You need to provide a valid number. Please try again. 4-40 Characters");
+    return passwordLength();
+  }
+  
+  };
+
+  passwordLength();
+
+  /* PASSWORD CRITERIA */
+  
   // asks user to select if special characters are wanted in generator
   var confirmSpecCharacters = window.confirm("Would you like to have special characters in your password?");
   if (confirmSpecCharacters === true){
     console.log("You selected to have special characters added to your password.");
+  
+    // *** Idea from Web Dev Simplified ***
+    confirmSpecCharacters = true;
   } else {
     console.log("Got it! No special characters.");
   }
@@ -156,6 +54,8 @@ generatePassword = function(){
   var confirmNumbers = window.confirm("Would you like to have numbers in your password?");
   if (confirmNumbers === true) {
     console.log("You selected to have numbers added to your password.");
+    // *** Idea from Web Dev Simplified ***
+    confirmNumbers = numbers;
   } else {
     console.log("Got it! No numbers.");
   }
@@ -164,6 +64,7 @@ generatePassword = function(){
   var confirmUpperCase = window.confirm("Would you like to have uppercase letters in your password?");
   if (confirmUpperCase === true) {
     console.log("You selected to have uppercase characters added to your password.");
+    confirmUpperCase = upperCase;
   } else {
     console.log("Got it! No uppercase characters.");
   }
@@ -172,21 +73,41 @@ generatePassword = function(){
   var confirmLowerCase = window.confirm("Would you like to have lowercase letters in your password?");
   if (confirmLowerCase === true) {
     console.log("You selected to have lowercase characters added to your password.");
+    confirmLowerCase = lowerCase;
   } else {
     console.log("Got it! No lowercase characters.");
   }
-
+  
   var confirmPassword = window.confirm("Are you ready to receive your password?");
   if (confirmPassword === true) {
     console.log("Here ya go!");
-    /*
-    * function goes here to call completed password
-     */
   } else {
     window.alert("Not a problem, maybe next time!");
   }
 
+ var generator = specialCharacter;
+  if (confirmLowerCase){
+   generator += lowerCase;
+  }
+  if (confirmNumbers) {
+    generator += numbers;
+  }
+  if (confirmUpperCase) {
+    generator += upperCase;
+  }
+
+  var password = [];
+  for (i=0; i < passwordLength.value; i++) {
+    var character = generator[Math.floor() * passwordLength.value];
+    password.push(String.fromCharCode(character));
+    
+  }
+  return password.join('');
+
 };
+  
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -195,7 +116,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
 
 
   passwordText.value = password;
