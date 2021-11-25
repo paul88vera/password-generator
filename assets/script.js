401d3716a0c7b75457df2166abe1acf4aaa97d40
paul88vera <paul88vera@gmail.com>
2021-11-25 00:33:49 -0600

    array has pushed arrays connected

--- assets/script.js
+++ assets/script.js
@@ -18,115 +18,107 @@ var specialChar = ["!","@","#","$","%","^","&","*","(",")",":",";",",","<",".","
 // numbers
 var numbers = ["1","2","3","4","5","6","7","8","9","0"];
 
-var characters = [""];
+var characters = [];
 
-/* PROMPTS USER FOR PASSWORD CRITERIA */
-
-var promptCriteria = function() {
-
-  // asks user to select if special characters are wanted in generator
-  var confirmSpecCharacters = window.confirm("Would you like to have special characters in your password?");
-  if (confirmSpecCharacters){
-    console.log("You selected to have special characters added to your password.");
-    confirmSpecCharacters === true;
-
-    // pushes array to characters array to be randomized
-    characters.concat(specialChar);
-    console.log(characters);
-
-  } else {
-    confirmSpecCharacters === false;
-    console.log("Got it! No special characters.");
-  }
-
-  // asks user to select if numbers are wanted in generator
-  var confirmNumbers = window.confirm("Would you like to have numbers in your password?");
-  if (confirmNumbers) {
-    console.log("You selected to have numbers added to your password.");
-    confirmNumbers === true;
-
-    // pushes array to characters array to be randomized
-    characters.concat(numbers);
-    console.log(characters);
-
-  } else {
-    confirmNumbers === false;
-    console.log("Got it! No numbers.");
-  }
-
-  // asks user to select if upperCase letters are wanted in generator
-  var confirmUpperCase = window.confirm("Would you like to have uppercase letters in your password?");
-  if (confirmUpperCase) {
-    console.log("You selected to have uppercase characters added to your password.");
-    confirmUpperCase === true;
-
-    // pushes array to characters array to be randomized
-    characters.concat(uppercaseLetters);
-    console.log(characters);
-
-  } else {
-    confirmUpperCase === false;
-    console.log("Got it! No uppercase characters.");
-  }
-
-  // asks user to select if lowercase letters are wanted in generator
-  var confirmLowerCase = window.confirm("Would you like to have lowercase letters in your password?");
-  if (confirmLowerCase) {
-    console.log("You selected to have lowercase characters added to your password.");
-    confirmLowerCase === true;
-
-    // pushes array to characters array to be randomized
-    characters.concat(lowercaseLetters);
-    console.log(characters);
-
-  } else {
-    confirmLowerCase === false;
-    console.log("Got it! No lowercase characters.");
-  }
-  
-  var confirmPassword = window.confirm("Are you ready to receive your password?");
-  if (confirmPassword) {
-    console.log("Here ya go!");
-    confirmPassword === true;
-  } else {
-    confirmPassword ===  false;
-    window.alert("Not a problem, maybe next time!");
-  }
-
-};
 
 /* GENERATES PASSWORD */
 
 var generatePassword = function(){
+/* PROMPTS USER FOR PASSWORD CRITERIA */
+
   var passwordTotal = prompt("How long would you like your password to be? 8 - 128 Characters");
   if (passwordTotal === "" || passwordTotal === null) {
     window.alert("You need to provide a valid number. Please try again. 8 - 128 Characters");
-    return promptCriteria();
+    passwordPrompt();
   }
   else if (passwordTotal >= 8 && passwordTotal <= 128) {
     console.log("Your password total length is: " + passwordTotal);
-    return promptCriteria();
+  } else if (!passwordTotal >= 8 && !passwordTotal <= 128) {
+    console.log("Your password total length is: " + passwordTotal);
+    passwordPrompt();
   }
 
+// asks user to select if special characters are wanted in generator
+var confirmSpecCharacters = window.confirm("Would you like to have special characters in your password?");
+if (confirmSpecCharacters === true){
+console.log("You selected to have special characters added to your password.");
+confirmSpecCharacters === true;
 
-  var passwordTotalLength = passwordTotal;
-  var pass = "";
+// pushes array to characters array to be randomized
+characters.push(...specialChar);
+console.log(characters);
+
+} else if (confirmSpecCharacters === false) {
+confirmSpecCharacters === false;
+console.log("Got it! No special characters.");
+}
+
+// asks user to select if numbers are wanted in generator
+var confirmNumbers = window.confirm("Would you like to have numbers in your password?");
+if (confirmNumbers === true) {
+console.log("You selected to have numbers added to your password.");
+confirmNumbers === true;
+
+// pushes array to characters array to be randomized
+characters.push(...numbers);
+console.log(characters);
+
+} else if (confirmNumbers === false) {
+confirmNumbers === false;
+console.log("Got it! No numbers.");
+}
+
+// asks user to select if upperCase letters are wanted in generator
+var confirmUpperCase = window.confirm("Would you like to have uppercase letters in your password?");
+if (confirmUpperCase === true) {
+console.log("You selected to have uppercase characters added to your password.");
+confirmUpperCase === true;
+
+// pushes array to characters array to be randomized
+characters.push(...uppercaseLetters);
+console.log(characters);
+
+} else if (confirmUpperCase === false) {
+confirmUpperCase === false;
+console.log("Got it! No uppercase characters.");
+}
+
+// asks user to select if lowercase letters are wanted in generator
+var confirmLowerCase = window.confirm("Would you like to have lowercase letters in your password?");
+if (confirmLowerCase === true) {
+console.log("You selected to have lowercase characters added to your password.");
+confirmLowerCase === true;
+
+// pushes array to characters array to be randomized
+characters.push(...lowercaseLetters);
+console.log(characters);
+
+} else if (confirmLowerCase === false) {
+confirmLowerCase === false;
+console.log("Got it! No lowercase characters.");
+}
+
+var confirmPassword = window.confirm("Are you ready to receive your password?");
+if (confirmPassword === true) {
+console.log("Here ya go!");
+confirmPassword === true;
+} else if (confirmPassword === false){
+confirmPassword ===  false;
+window.alert("Not a problem, maybe next time!");
+}
+
+// password randomizer
+var length = passwordTotal;
+var pass = "";
+
+for (let i =0; i < length; i++) {
+  var pass = characters[Math.floor(Math.random() * characters.length)];
   
-  for (var i=0; i < passwordTotalLength; i++) {
-    var randomNumber = characters[Math.floor(Math.random() * characters.length)];
-    var pass = characters.push(randomNumber, randomNumber);
-    
-  }
-
+};
   document.getElementById("password").value = pass;
-  return pass;
+  console.log(pass);
 
 };
-
-// characters.push(numbers);
-// characters.push(specialChar);
-// characters.push(uppercaseLetters);
-// characters.push(lowercaseLetters);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
